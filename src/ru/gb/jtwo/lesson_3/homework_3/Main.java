91b4ee922c0f9d21330c56fc7ffa2c729914249d
sulla <inikolazzz@gmail.com>
2020-04-14 14:34:11 +0300

    end of homework_3

--- src/ru/gb/jtwo/lesson_3/homework_3/Main.java
+++ src/ru/gb/jtwo/lesson_3/homework_3/Main.java
@@ -4,37 +4,68 @@ import java.util.*;
 
 public class Main {
 
-    private static String[] ArrayOfStrings = {"революция", "цель", "король", "низложен", "революция", "катящийся",
+    private static final String[] words = {"революция", "цель", "король", "низложен", "революция", "катящийся",
             "шар", "умеренные", "народ", "коммуна", "страх", "цель", "народ", "революция", "отстранить",
             "символ", "символ", "коммуна", "сжечь", "прах"};
 
     public static void main(String[] args) {
 
-        Set<String> mySet = new HashSet<>();
-        Collections.addAll(mySet,ArrayOfStrings);
-        System.out.println(mySet);
-        countEachMapWord(ArrayOfStrings);
+        System.out.println(getWords(words));
+        System.out.println(getWordsCount(words));
 
+        PhoneBook phonebook = new PhoneBook();
+        phonebook.add("Ivanov", "ivanov-phone-1", "ivanov-mail-1");
+        phonebook.add("Petrov", "petrov-phone-1", "petrov-mail-1");
+        phonebook.add("Ivanov", "ivanov-phone-2", "ivanov-mail-2");
 
+        System.out.println("e-mail Ivanov: " + phonebook.getMails("Ivanov"));
+        System.out.println("e-mail Petrov: " + phonebook.getMails("Petrov"));
+        System.out.println("phone Ivanov: " + phonebook.getPhones("Ivanov"));
+        System.out.println("phone Petrov: " + phonebook.getPhones("Petrov"));
 
+//        Set<String> mySet = new HashSet<>();
+//        Collections.addAll(mySet,ArrayOfStrings);
+//        System.out.println(mySet);
+//        countEachMapWord(ArrayOfStrings);
     }
 
-    public static  Map<String, Integer> countEachMapWord (String[] array) {
 
-        Map<String,Integer > map = new HashMap<>();
-        for (int i = 0; i <ArrayOfStrings.length ; i++) {
-            if(map.containsKey(ArrayOfStrings[i])) {
-                map.put(ArrayOfStrings[i], map.get(ArrayOfStrings[i]) +1);
+    private static TreeSet<String> getWords(String[] arr) {
+        return new TreeSet<>(Arrays.asList(arr));
+    }
+
+    private static HashMap<String, Integer> getWordsCount(String[] arr) {
+        HashMap<String, Integer> map = new HashMap<>();
+        for (int i = 0; i < arr.length; i++) {
+            String word = arr[i];
+            if (map.containsKey(word)) {
+                map.put(word, map.get(word) + 1);
             } else {
-                map.put(ArrayOfStrings[i], 1);
+                map.put(word, 1);
             }
         }
-
-        for (Map.Entry<String,Integer> pair: map.entrySet()) {
-            System.out.println(pair.getKey() + " = " + pair.getValue());
-        }
-
         return map;
     }
+
+
+
+
+//    public static  Map<String, Integer> countEachMapWord (String[] array) {
+//
+//        Map<String,Integer > map = new HashMap<>();
+//        for (int i = 0; i <ArrayOfStrings.length ; i++) {
+//            if(map.containsKey(ArrayOfStrings[i])) {
+//                map.put(ArrayOfStrings[i], map.get(ArrayOfStrings[i]) +1);
+//            } else {
+//                map.put(ArrayOfStrings[i], 1);
+//            }
+//        }
+//
+//        for (Map.Entry<String,Integer> pair: map.entrySet()) {
+//            System.out.println(pair.getKey() + " = " + pair.getValue());
+//        }
+//
+//        return map;
+//    }
 
 }
--- src/ru/gb/jtwo/lesson_3/homework_3/Person.java
+++ src/ru/gb/jtwo/lesson_3/homework_3/Person.java
@@ -3,20 +3,12 @@ package ru.gb.jtwo.lesson_3.homework_3;
 
 public class Person {
 
-    private String phoneNumber;
-    private String email;
+    public String phone;
+    public String email;
 
     public Person(String phoneNumber, String email) {
-        this.phoneNumber = phoneNumber;
+        this.phone = phoneNumber;
         this.email = email;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public String getEmail() {
-        return email;
     }
 }
 
